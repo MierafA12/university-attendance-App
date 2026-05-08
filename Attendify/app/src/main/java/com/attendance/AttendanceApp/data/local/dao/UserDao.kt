14c01f0f1799509433680b9ac233e4668e5df7f4
tsedeniayeshibelay <etsedon71@gmail.com>
2026-05-08 00:42:40 -0700

    created a user dao

---
+++ Attendify/app/src/main/java/com/attendance/AttendanceApp/data/local/dao/UserDao.kt
@@ -0,0 +1,41 @@
+package com.attendance.attendanceapp.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.attendance.attendanceapp.data.local.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertUser(user: UserEntity): Long
+
+    @Update
+    suspend fun updateUser(user: UserEntity)
+
+    @Delete
+    suspend fun deleteUser(user: UserEntity)
+
+    @Query("SELECT * FROM users WHERE id = :id")
+    fun getUserById(id: Int): Flow<UserEntity?>
+
+    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+    suspend fun getUserByEmail(email: String): UserEntity?
+
+    @Query("SELECT * FROM users WHERE email = :email AND password = :password LIMIT 1")
+    suspend fun login(email: String, password: String): UserEntity?
+
+    @Query("SELECT * FROM users WHERE role = :role")
+    fun getUsersByRole(role: String): Flow<List<UserEntity>>
+
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): Flow<List<UserEntity>>
+
+    @Query("DELETE FROM users WHERE id = :id")
+    suspend fun deleteUserById(id: Int)
+}
