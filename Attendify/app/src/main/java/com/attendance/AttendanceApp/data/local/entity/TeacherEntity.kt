23b04c0e53f61002d27d310cad2266d02628edd7
Lidia Shenkut <liduuruha@gmail.com>
2026-05-11 10:08:59 -0700

    fixed the teachers entity

--- Attendify/app/src/main/java/com/attendance/AttendanceApp/data/local/entity/TeacherEntity.kt
+++ Attendify/app/src/main/java/com/attendance/AttendanceApp/data/local/entity/TeacherEntity.kt
@@ -11,28 +11,14 @@ import androidx.room.PrimaryKey
  */
 @Entity(
     tableName = "teachers",
-    foreignKeys = [
-        ForeignKey(
-            entity = UserEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["userId"],
-            onDelete = ForeignKey.CASCADE
-        ),
-        ForeignKey(
-            entity = DepartmentEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["departmentId"],
-            onDelete = ForeignKey.SET_NULL
-        )
-    ],
     indices = [
         Index(value = ["userId"], unique = true),
         Index(value = ["departmentId"])
     ]
 )
 data class TeacherEntity(
-    @PrimaryKey(autoGenerate = true) val teacherId: Int = 0,
-    val userId: Int,
-    val departmentId: Int?,
+    @PrimaryKey val teacherId: String,
+    val userId: String,
+    val departmentId: String?,
     val specialization: String
 )
