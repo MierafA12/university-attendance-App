232405927c14cc4f194ef9fc4181bcae81402c91
Lidia Shenkut <liduuruha@gmail.com>
2026-05-11 10:07:13 -0700

    fixed the schedule entity

--- Attendify/app/src/main/java/com/attendance/AttendanceApp/data/local/entity/ScheduleEntity.kt
+++ Attendify/app/src/main/java/com/attendance/AttendanceApp/data/local/entity/ScheduleEntity.kt
@@ -1,44 +1,16 @@
 package com.attendance.attendanceapp.data.local.entity
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(
-    tableName = "schedules",
-    foreignKeys = [
-        ForeignKey(
-            entity = CourseEntity::class,
-            parentColumns = ["courseId"],
-            childColumns = ["courseId"],
-            onDelete = ForeignKey.CASCADE
-        ),
-        ForeignKey(
-            entity = TeacherEntity::class,
-            parentColumns = ["teacherId"],
-            childColumns = ["teacherId"],
-            onDelete = ForeignKey.CASCADE
-        ),
-        ForeignKey(
-            entity = DepartmentEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["departmentId"],
-            onDelete = ForeignKey.CASCADE
-        )
-    ],
-    indices = [
-        Index(value = ["courseId"]),
-        Index(value = ["teacherId"]),
-        Index(value = ["departmentId"])
-    ]
-)
+@Entity(tableName = "schedules")
 data class ScheduleEntity(
-    @PrimaryKey(autoGenerate = true) val scheduleId: Int = 0,
-    val courseId: Int,
-    val teacherId: Int,
-    val departmentId: Int,
+    @PrimaryKey val scheduleId: String,
+    val courseId: String,
+    val teacherId: String,
+    val departmentId: String,
     val year: String,
+    val semester: String,
     val dayOfWeek: String,
     val startTime: String,
     val endTime: String
